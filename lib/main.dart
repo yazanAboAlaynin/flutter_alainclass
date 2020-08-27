4807092510e9d932be9279e2c12a90320e41ca1c
YazanAboAlaynin <yazanmohned@gmail.com>
2020-08-27 14:14:50 +0300

    notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-import 'dart:io';
-
-import 'package:alainclass/repositories/repositories.dart';
 import 'package:alainclass/screens/start_video.dart';
 import 'package:bloc/bloc.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_app_badger/flutter_app_badger.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:onesignal/onesignal.dart';
 
-import 'blocs/blocs.dart';
 import 'blocs/simple_bloc_observer.dart';
-import 'package:http/http.dart' as http;
 
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
@@ -43,10 +38,171 @@ class AlainClass extends StatefulWidget {
 class _AlainClassState extends State<AlainClass> {
   String title = "Title";
   String helper = "helper";
+  String _debugLabelString = "";
+  String _emailAddress;
+  String _externalUserId;
+  bool _enableConsentButton = false;
 
+  // CHANGE THIS parameter to true if you want to test GDPR privacy consent
+  bool _requireConsent = true;
   @override
   void initState() {
     super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    // if (!mounted) return;
+
+    // OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+    // OneSignal.shared.setRequiresUserPrivacyConsent(_requireConsent);
+
+    var settings = {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.promptBeforeOpeningPushUrl: true
+    };
+
+    // OneSignal.shared.setNotificationReceivedHandler((notification) {
+    //   this.setState(() {
+    //     _debugLabelString =
+    //         "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+    //   });
+    // });
+
+    // OneSignal.shared
+    //     .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+    //   this.setState(() {
+    //     _debugLabelString =
+    //         "Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+    //   });
+    // });
+
+    // OneSignal.shared
+    //     .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+    //   print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+    // });
+
+    // OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+    //   print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+    // });
+
+    // OneSignal.shared.setEmailSubscriptionObserver(
+    //     (OSEmailSubscriptionStateChanges changes) {
+    //   print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+    // });
+
+    // NOTE: Replace with your own app ID from https://www.onesignal.com
+    await OneSignal.shared
+        .init("b8943e33-4e33-41bf-99f3-8e18d0070ca3", iOSSettings: settings);
+
+    // OneSignal.shared
+    //     .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+    // bool requiresConsent = await OneSignal.shared.requiresUserPrivacyConsent();
+
+    // this.setState(() {
+    //   _enableConsentButton = requiresConsent;
+    // });
+  }
+
+  void _handleGetTags() {
+    OneSignal.shared.getTags().then((tags) {
+      if (tags == null) return;
+
+      setState((() {
+        _debugLabelString = "$tags";
+      }));
+    }).catchError((error) {
+      setState(() {
+        _debugLabelString = "$error";
+      });
+    });
+  }
+
+  void _handleSendTags() {
+    print("Sending tags");
+    OneSignal.shared.sendTag("test2", "val2").then((response) {
+      print("Successfully sent tags with response: $response");
+    }).catchError((error) {
+      print("Encountered an error sending tags: $error");
+    });
+  }
+
+  void _handlePromptForPushPermission() {
+    print("Prompting for Permission");
+    OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+      print("Accepted permission: $accepted");
+    });
+  }
+
+  void _handleGetPermissionSubscriptionState() {
+    print("Getting permissionSubscriptionState");
+    OneSignal.shared.getPermissionSubscriptionState().then((status) {
+      this.setState(() {
+        _debugLabelString = status.jsonRepresentation();
+      });
+    });
+  }
+
+  void _handleSetEmail() {
+    if (_emailAddress == null) return;
+
+    print("Setting email");
+
+    OneSignal.shared.setEmail(email: _emailAddress).whenComplete(() {
+      print("Successfully set email");
+    }).catchError((error) {
+      print("Failed to set email with error: $error");
+    });
+  }
+
+  void _handleLogoutEmail() {
+    print("Logging out of email");
+    OneSignal.shared.logoutEmail().then((v) {
+      print("Successfully logged out of email");
+    }).catchError((error) {
+      print("Failed to log out of email: $error");
+    });
+  }
+
+  void _handleConsent() {
+    print("Setting consent to true");
+    OneSignal.shared.consentGranted(true);
+
+    print("Setting state");
+    this.setState(() {
+      _enableConsentButton = false;
+    });
+  }
+
+  void _handleSetLocationShared() {
+    print("Setting location shared to true");
+    OneSignal.shared.setLocationShared(true);
+  }
+
+  void _handleDeleteTag() {
+    print("Deleting tag");
+    OneSignal.shared.deleteTag("test2").then((response) {
+      print("Successfully deleted tags with response $response");
+    }).catchError((error) {
+      print("Encountered error deleting tag: $error");
+    });
+  }
+
+  void _handleSetExternalUserId() {
+    print("Setting external user ID");
+    OneSignal.shared.setExternalUserId(_externalUserId);
+    this.setState(() {
+      _debugLabelString = "Set External User ID";
+    });
+  }
+
+  void _handleRemoveExternalUserId() {
+    OneSignal.shared.removeExternalUserId();
+    this.setState(() {
+      _debugLabelString = "Removed external user ID";
+    });
   }
 
   @override
